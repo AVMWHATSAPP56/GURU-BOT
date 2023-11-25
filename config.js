3c6afe1f99830b9ec8d13cb6690f2b24dd37832d
AVMWHATSAPP56 <127578094+AVMWHATSAPP56@users.noreply.github.com>
2023-11-25 07:26:49 +0500

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['917605902011', 'GURU', true],
-  ['6281255369012', 'Ash', true], 
+  ['923107666558', 'GURU', true],
+  ['923107666558', 'King', true], 
   [''] 
 ] //Number of owners
 
